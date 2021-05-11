e7ae2496bd61a94a87ae1fd5da1170ca00761b0f
faizp <faizkizhisseri@gmail.com>
2021-05-11 11:50:45 +0530

    Created Questions

--- admins/urls.py
+++ admins/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('instructor-test/', views.instructor_test, name='instructor-test'),
     path('category-test/<int:id>', views.category_test, name='category-test'),
     path('questions/<int:id>', views.questions, name='questions'),
-    path('answers/<int:id>', views.answers, name='answers')
+    path('answers/<int:id>', views.answers, name='answers'),
+    path('add-question/', views.add_question, name='add-question'),
+    path('choose-category/', views.choose_category, name='choose-category')
 ]
--- admins/views.py
+++ admins/views.py
@@ -54,7 +54,6 @@ def sec_category(request):
 
 def instructor_test(request):
     categories = Category.objects.all()
-    print(categories)
     context = {
         'categories': categories
     }
@@ -85,9 +84,69 @@ def answers(request, id):
     question = Questions.objects.get(id=id)
     answers = Answer.objects.filter(question=question)
     correct_answer = Answer.objects.filter(question=question, correct=True)
-    print(correct_answer)
     data = {
         'answers': serializers.serialize('json', answers),
         'correctAnswer': serializers.serialize('json', correct_answer)
     }
     return JsonResponse(data)
+
+
+def add_question(request):
+    if request.method == 'POST':
+        sub_category = request.POST['subCategory']
+        question1 = request.POST['question1']
+        answer1 = request.POST['answer1']
+        answer2 = request.POST['answer2']
+        answer3 = request.POST['answer3']
+        answer4 = request.POST['answer4']
+        answeropt1 = request.POST['answeropt1']
+        if answeropt1 == 'true':
+            answeropt1 = True
+        else:
+            answeropt1 = False
+        answeropt2 = request.POST['answeropt2']
+        if answeropt2 == 'true':
+            answeropt2 = True
+        else:
+            answeropt2 = False
+        answeropt3 = request.POST['answeropt3']
+        if answeropt3 == 'true':
+            answeropt3 = True
+        else:
+            answeropt3 = False
+        answeropt4 = request.POST['answeropt4']
+        if answeropt4 == 'true':
+            answeropt4 = True
+        else:
+            answeropt4 = False
+        count = 0
+        lis = [answeropt1, answeropt2, answeropt3, answeropt4]
+        for i in range(len(lis)):
+            if lis[i] == 'true':
+                count += 1
+        if count>1:
+            return JsonResponse('false', safe=False)
+        sec_category = secCategory.objects.get(id=sub_category)
+        question_created = Questions.objects.create(sec_category=sec_category, question=question1)
+        Answer.objects.create(question=question_created, answer=answer1, correct=answeropt1)
+        Answer.objects.create(question=question_created, answer=answer2, correct=answeropt2)
+        Answer.objects.create(question=question_created, answer=answer3, correct=answeropt3)
+        Answer.objects.create(question=question_created, answer=answer4, correct=answeropt4)
+        return JsonResponse('true', safe=False)
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'admins/add-question.html', context)
+
+
+def choose_category(request):
+    if request.method == 'POST':
+        category = request.POST['category']
+        sec_categories = secCategory.objects.filter(category=category)
+        data = {
+            'categories': serializers.serialize('json', sec_categories)
+        }
+        return JsonResponse(data)
+    return JsonResponse('false', safe=False)
+        
